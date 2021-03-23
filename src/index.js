d5430fda9789f9b4559c505e9f75dcd4c742a2ba
rnarcos <marcos@fernandescouto.com>
2021-03-23 01:36:33 -0300

    # Index: . Render css reset component.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
+import { GlobalStyles } from './styles';
 import App from './components/App';
 
 ReactDOM.render(
   <Provider store={store}>
+    <GlobalStyles />
     <App />
   </Provider>,
   document.getElementById('root'),
