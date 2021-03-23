da77f49c12dcf7cf2ec79686772ea0a41599a936
rnarcos <marcos@fernandescouto.com>
2021-03-23 01:33:58 -0300

    # Index: . Render App instead of boilerplate jsx.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
+import App from './components/App';
 
 ReactDOM.render(
   <Provider store={store}>
-    <h1>Hello world!</h1>
+    <App />
   </Provider>,
   document.getElementById('root'),
 );
