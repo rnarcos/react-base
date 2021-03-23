2b92b6954229ef724512599746d117ba9c21226c
rnarcos <marcos@fernandescouto.com>
2021-03-23 01:25:47 -0300

    # Index: . Wrap hello world boilerplate within store provider.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
-ReactDOM.render(<h1>Hello world!</h1>, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <h1>Hello world!</h1>
+  </Provider>,
+  document.getElementById('root'),
+);
