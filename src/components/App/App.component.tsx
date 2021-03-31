7d88b84fcf99807f7d1db04af2bfb58179ae3b67
rnarcos <marcos@fernandescouto.com>
2021-03-30 22:04:15 -0300

    # App: . Create App component, and it's type. . Create exports.

---
+++ src/components/App/App.component.tsx
@@ -0,0 +1,6 @@
+import React, { FC } from 'react';
+import { Props } from './App.props';
+
+const App: FC<Props> = () => <h1>Hello world!</h1>;
+
+export default App;
---
+++ src/components/App/index.ts
@@ -0,0 +1,2 @@
+export { default } from './App.component';
+export { Props as AppProps } from './App.props';
